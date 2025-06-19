899b333ed241dcdef3352a4ea9184d44ead5ea1c
YKH2020 <yash.hegde.2020@gmail.com>
2025-06-19 17:18:03 -0400

    Added AWS Knowledge Base and different local model

--- scripts/load_docs.py
+++
@@ -1,6 +0,0 @@
-from langchain_community.document_loaders import PyPDFDirectoryLoader
-
-def load_docs(path: str) -> PyPDFDirectoryLoader:
-    """Load PDF documents and print its content."""
-    loader = PyPDFDirectoryLoader(path)
-    return loader.load()
